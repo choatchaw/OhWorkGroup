abfeb4ac4830813293fa97baea04bdab3a3a5851
Firore <takbasic722@gmail.com>
2016-06-13 22:27:45 +0700

    test
    testtest

---
+++ page2.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+	<body>
+		<div class='warpper'>
+		<div class='row container gpadding'>
+			<div class='col-md-12 gamecontent gpadding'>
+				<div class='col-md-6 gameheadleft'>
+					<div class='col-md-1 contact gpadding'>
+					<p>Contact</p>
+					</div>
+					<div class='col-md-1 sitemap gpadding'>
+					<p>Sitemap</p>
+					</div>
+				</div>
+				<div class='col-md-6 gameheadright'>
+					<div class='col-md-8'>
+					</div>
+					<div class='col-md-2 login'>
+					<p>Login</p>
+					</div>
+					<div class='col-md-2 register'>
+					<p>Register</p>
+					</div>
+				</div>
+				<div class='col-md-12 filter'>
+                	<div class='col-md-12 filtermenu'>
+                    	<div class='col-md-3 filtertop'>
+                        	 <p>PS</p>
+                        </div>
+                        <div class='col-md-3 filtertop x'>
+                        	<p>XBOX</p>
+                        </div>
+                        <div class='col-md-3 filtertop w'>
+                        	<p>WII</p>
+                        </div>
+                        <div class='col-md-3 filtertop fk'>
+                        	<p>FK</p>
+                        </div>
+                    </div>
+                    <div class='col-md-12 filtersubmenu'>
+                    	<div class='col-md-3 submenu c'>
+                        </div>
+                        <div class='col-md-3 submenu d'>
+                        </div>
+                        <div class='col-md-3 submenu a'>
+                        </div>
+                        <div class='col-md-3 submenu e'>
+                        </div>
+                    </div>
+				</div>
+				<div class='col-md-12 show gpadding'>
+					<div class='col-md-12 showrow1 gpadding'>
+						<div class='thumbnail col-md-3 showbox gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption showtext">
+                                    <h3>Topic</h3>
+                                    <p>text</p>
+                                    <p>฿500</p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+					</div>
+					<div class='col-md-12 showrow2 gpadding'>
+						<div class='thumbnail col-md-3 showbox gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>					</div>
+					<div class='col-md-12 showrow2 gpadding'>
+						<div class='thumbnail col-md-3 showbox gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+					</div>
+					<div class='col-md-12 showrow2 gpadding'>
+						<div class='thumbnail col-md-3 showbox gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+						<div class='thumbnail col-md-3 showbox2 gpadding'>
+                 			 <img src="images/nopic.png">
+                               	<div class="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                                </div>
+                        </div>
+					</div>
+                    <div class='col-md-12 footer'>
+                    </div>
+				</div>
+			</div>
+		</div><!--row-->
+		</div><!--warpper-->
+	</body>		
+</html>
